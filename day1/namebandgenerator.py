90c1893c9b6f325d54de1c9e2ff3e3912369e92f
Gabin Longoni <gabin.longoni@gmail.com>
2024-02-02 01:01:38 +0100

    Day 1

---
+++ day1/namebandgenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Generator")
+cityName = input("What's the name of the city you grew up in ?\n")
+petName = input("What's the name of your personal pet ?\n")
+print("Your band name is: " + cityName + " " + petName)
